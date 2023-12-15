d9b79ed24b32fcef79e6c5e8053905da1913a86a
Akira <37931@ma-web.nl>
2023-12-15 10:17:05 +0100

    canvas02

--- 02 canvashuis/app.js
+++ 02 canvashuis/app.js
@@ -6,7 +6,12 @@ class App
         let canvas =document.getElementById("canvasId");
         console.log(canvas);
         let g = canvas.getContext("2d");
+        g.fillStyle = "blue";
+        g.fillRect(0,0,300,300)
+        g.fillStyle = "yellow";
         g.fillRect(0,0,10,10);
+        
+        
 
 
     }
