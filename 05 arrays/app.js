49e94a2431d40f5dbf3d98b3b9ae36c20251b958
Akira <37931@ma-web.nl>
2024-01-26 10:48:05 +0100

    array

--- 05 arrays/app.js
+++ 05 arrays/app.js
@@ -2,15 +2,16 @@
 class App {
     runApplication() {
         console.log("hello world!");
+        let headers =  document.getElementsByTagName("h1");
+        
     }
+
 }
 
 let app = new App();
 app.runApplication();
 
-
 let artist = ["artist1", "artist2", "artist3", "etc"];
-
 
 console.log(artist);
 
@@ -19,8 +20,9 @@ for (let i = 0; i < length; i++) {
     
 }
 
-let nummers = [2, 5, 7];
 
+
+let nummers = [2, 5, 7];
 
 console.log(nummers);
 
@@ -28,8 +30,6 @@ for (let i = 0; i < nummers.length; i++) {
     nummers[i] += 1;
     console.log(nummers[i]); 
 }
-
-let numeros = [2, 5, 7];
 
 for (let i = 0; i < nummers.length; i++) {
     nummers[i] += i; 
@@ -44,7 +44,6 @@ for (let i = 0; i < artists.length; i++) {
 }
 
 artists.push("The Cure", "Elton John");
-
 
 for (let i = 0; i < artists.length; i++) {
     console.log(artists[i]);
